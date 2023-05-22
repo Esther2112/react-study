489dc4b9dbfd1639420643d29140d631c2b177b3
Jin Han <120617717+Esther2112@users.noreply.github.com>
2023-05-22 10:42:56 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import SayHello from "./SayHello";
 
 function App() {
+
+    const looping = () => {
+        const helloList =[];
+        for(let i = 0; i < 5; i++) {
+            helloList.push(<SayHello />);
+        }
+        return helloList;
+    }
+
+    //jsx 문법( return() 내부 )에서는 스크립트 코드를 직접 사용이 불가능
+    //{}로 묶어서 함수호출이나 변수참조가 가능하고 세미콜론 생략하는 문법으로 사용
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        {looping()}
+    </>
   );
 }
 
